ecfed887165774da401dbea7a7593667cab6fcf5
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-10 18:35:22 +0100

    working on password

--- library_management/library_api/models.py
+++ library_management/library_api/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.hashers import make_password
 
 # Create your models here.
 from django.db import models
@@ -19,6 +20,13 @@ class User(models.Model):
     user_id = models.CharField(max_length=10, unique=True, null=False)  
     department = models.CharField(max_length=100)
     identification = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, default=STUDENT) 
+    password = models.CharField(max_length=15, blank=False)
 
     def __str__(self):
         return f"{self.name} ({self.user_id}) ({self.identification})"
+    
+ def save(self, *args, **kwargs):
+        # Hash the password before saving
+        if self.password:
+            self.password = make_password(self.password)
+        super().save(*args, **kwargs)
