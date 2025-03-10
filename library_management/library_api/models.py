9fe74ea5cf288fe8f4e7a7b781aacfec5615f026
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-10 21:56:52 +0100

    working on password count

--- library_management/library_api/models.py
+++ library_management/library_api/models.py
@@ -20,13 +20,16 @@ class User(models.Model):
     user_id = models.CharField(max_length=10, unique=True, null=False)  
     department = models.CharField(max_length=100)
     identification = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, default=STUDENT) 
-    password = models.CharField(max_length=15, blank=False)
+    password = models.CharField(max_length=15, default="defaultpassword")
 
     def __str__(self):
         return f"{self.name} ({self.user_id}) ({self.identification})"
     
- def save(self, *args, **kwargs):
-        # Hash the password before saving
-        if self.password:
+def save(self, *args, **kwargs):
+        if self.password and not self.pk:
             self.password = make_password(self.password)
         super().save(*args, **kwargs)
+
+        
+def check_password(self, raw_password):
+        return check_password(raw_password, self.password)
