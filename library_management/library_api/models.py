b22dff82021a82eb32e1db83f9d0ca81c452e1ad
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-10 18:11:04 +0100

    defining the user type admin or student or staff

--- library_management/library_api/models.py
+++ library_management/library_api/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class User(models.Model):
+    STUDENT = 'student'
+    STAFF = 'staff'
+    ADMIN = 'admin'
+    USER_TYPE_CHOICES = [
+        (STUDENT, 'Student'),
+        (STAFF, 'Staff'),
+        (ADMIN, 'Admin'),
+    ]
+
+    # Define fields here
+    name = models.CharField(max_length=100, blank=True) 
+    email = models.CharField(unique=True, max_length=100) 
+    user_id = models.CharField(max_length=10, unique=True, null=False)  
+    department = models.CharField(max_length=100)
+    identification = models.CharField(max_length=10, choices=USER_TYPE_CHOICES, default=STUDENT) 
+
+    def __str__(self):
+        return f"{self.name} ({self.user_id}) ({self.identification})"
