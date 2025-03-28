62b3e9cbbd8958a4e5adf572a0d5408f0200edc7
Ugwu Gerard Onyedikachi <kachimaxy1@gmail.com>
2025-03-28 12:24:40 +0100

    user model for books api

--- library_management/books/models.py
+++ library_management/books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Books(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    publication_date = models.DateField()
+    isbn_number = models.CharField(max_length=13, unique=True)
+    genre = models.CharField(max_length=50)
+    copies_available = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.title
